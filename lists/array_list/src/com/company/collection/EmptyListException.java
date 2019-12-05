fd73655bf6dc9a446d2106c9516dc67a95dcea52
andre.reutow@gmail.com <andre.reutow@gmail.com>
2019-12-05 13:50:29 +0100

    update

---
+++ lists/array_list/src/com/company/collection/EmptyListException.java
@@ -0,0 +1,5 @@
+package com.company.collection;
+
+class EmptyListException extends RuntimeException {
+
+}
---
+++ lists/array_list/src/com/company/comparator/IntegerComparator.java
@@ -0,0 +1,18 @@
+package com.company.comparator;
+
+
+public class IntegerComparator extends AbstractComparator {
+
+
+    @Override
+    public int compare(Object o1, Object o2) {
+        Integer i1 = (Integer) o1;
+        Integer i2 = (Integer) o2;
+
+//        if (i1 < i2)
+//            return -1;
+//        else if (i1 > i2)
+//            return 1;
+        return i1 - i2;
+    }
+}
