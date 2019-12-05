f9841f45e38d550796390da472c739390af7321b
andre.reutow@gmail.com <andre.reutow@gmail.com>
2019-12-05 12:34:51 +0100

    add Variables

--- Basic/variables/src/com/company/Variables.java
+++ Basic/variables/src/com/company/Variables.java
@@ -1,23 +1,17 @@
 package com.company;
 
-import java.util.Scanner;
-
 public class Variables {
 
     public static void main(String[] args) {
 
-//region задача
         int petja = 5;
         int vasja = 5* petja;
         int maria = vasja - 7;
 
         int all = petja + vasja + maria;
         int ma = maria+ petja;
-//endregion
-//region вывод
         System.out.println( "\nAre apples: " + all +
                             "\nApples Maria and Petja: " + ma);
-//endregion
 
     }
 }
