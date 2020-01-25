67b38ce978152af4ef7d918bfc842a7d947caf5a
andre.reutow@gmail.com <andre.reutow@gmail.com>
2020-01-25 20:10:51 +0100

    adds HashMap and Tests from 9morning

--- lists/hashmap/src/com/company/Application.java
+++ lists/hashmap/src/com/company/Application.java
@@ -1,8 +1,40 @@
 package com.company;
 
+import com.company.performance_test.JavaHashMapPerformsRule;
+import com.company.performance_test.NewHashMapPerformsRule;
+import com.company.performance_test.OurHashMapPerformsRule;
+import com.company.performance_test.PerformanceTest;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Application {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws FileNotFoundException {
+        Date dateNow = new Date();
+        SimpleDateFormat formatForDateNow = new SimpleDateFormat("hh:mm:ss:S");
+
+        PerformanceTest testJava = new JavaHashMapPerformsRule();
+        PerformanceTest testOur = new OurHashMapPerformsRule();
+        PerformanceTest testNew = new NewHashMapPerformsRule();
+
+
+        int n = 580000;
+        String format = "%1s|%2d|%2s|%2d|%2s|%2d\n";
+
+
+            System.out.println("Put:");
+            System.out.format(format, "Java", testJava.performPut(n), "Our", testOur.performPut(n), "New", testNew.performPut(n));
+            System.out.println("Get:");
+            System.out.printf(format, "Java", testJava.performGet(), "Our", testOur.performGet(), "New", testNew.performGet());
+            System.out.println("Content:");
+            System.out.printf(format, "Java", testJava.performContent(), "Our", testOur.performContent(), "New", testNew.performContent());
+            System.out.println("Remove");
+            System.out.printf(format, "Java", testJava.performRemove(), "Our", testOur.performRemove(), "New", testNew.performRemove());
+            System.out.println();
+
     }
 }
