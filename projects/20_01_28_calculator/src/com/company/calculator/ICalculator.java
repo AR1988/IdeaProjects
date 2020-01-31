3366143f01699d0bbec556c450f97f11c511cb89
andre.reutow@gmail.com <andre.reutow@gmail.com>
2020-01-31 16:11:27 +0100

    merge

--- projects/20_01_28_calculator/src/com/company/calculator/ICalculator.java
+++
@@ -1,8 +0,0 @@
-package com.company.calculator;
-
-import com.company.exception.OperationNotSupported;
-
-public interface ICalculator {
-
-    double calculate(double num1, double num2, char operand) throws OperationNotSupported;
-}
