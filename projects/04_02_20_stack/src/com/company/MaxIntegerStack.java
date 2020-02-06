a299591b42fa5506038d3d6c99136cf61ca50739
AR1988 <AudiTT2012>
2020-02-06 09:29:42 +0100

    stack: edit List to Deque

---
+++ projects/04_02_20_stack/src/com/company/MaxIntegerStack.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.util.*;
+
+public class MaxIntegerStack {
+
+    Deque<Integer> source = new ArrayDeque<>();
+    Deque<Integer> maxSource = new ArrayDeque<>();
+    Comparator comparator;
+
+    public MaxIntegerStack() {
+
+    }
+
+    public void addLast(Integer elt) {
+        source.addLast(elt);
+        if (size() == 0 || elt > max())
+            maxSource.addLast(elt);
+        else
+            maxSource.addLast(max());
+    }
+
+    public Integer getLast() {
+        return source.getLast();
+    }
+
+    public int size() {
+        return source.size();
+    }
+
+    public Integer removeLast() {
+        maxSource.removeLast();
+        return source.removeLast();
+    }
+
+    public Integer max() {
+        return maxSource.getLast();
+    }
+}
