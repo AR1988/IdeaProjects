f88ec4dc5a68552c3cd67299e13fbf6b3f075253
andre.reutow@gmail.com <andre.reutow@gmail.com>
2020-02-06 09:14:37 +0100

    rename and clear Main(Application)

--- interview/25_01_20_interview/src/com/company/Application.java
+++ interview/25_01_20_interview/src/com/company/Application.java
@@ -1,10 +1,12 @@
 package com.company;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
-public class Main {
+public class Application {
 
-    public static void main(String[] args) throws IOException {
+    public static void Application(String[] args) throws IOException {
 //        1. определить, является ли строка палиндромом. т.е. читается одинаково и с начала и с конца
 //        2. есть лист целых чисел. и есть еще одно число n. определить, найдется ли два числа из листа, в сумме дающие n
 //        3. на вход дается строка. на выходе вернуть строку, в которой все заглавные буквы превратились в прописные, а прописные в заглавные. строка может содержать любые символы. например Hello1-＞hELLO1
