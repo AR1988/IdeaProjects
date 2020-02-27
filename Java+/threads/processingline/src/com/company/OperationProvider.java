4ebea0065997eef68c728c3d8d7e6be3849d1aac
AR1988 <AudiTT2012>
2020-02-27 19:11:57 +0100

    update

--- Java+/threads/processingline/src/com/company/OperationProvider.java
+++ Java+/threads/processingline/src/com/company/OperationProvider.java
@@ -1,10 +1,10 @@
 package com.company;
 
-import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
 
 public class OperationProvider {
 
-//    private final Map<String, IOperation> operationByName;
+    private final Map<String, IOperation> operationByName;
 
 
     private static final String OPERATIONS_PACKAGE = "com.company.operation.";
@@ -13,30 +13,29 @@ public class OperationProvider {
 //        operationByName.put("lowercase", new LowerCaseOperation());
 //        operationByName.put("uppercase", new UpperCaseOperation());
 
-//    public OperationProvider(Map<String, IOperation> operationByName) {
-//        this.operationByName = operationByName;
-//    }
-
-    /**
-     * @param operationName name of operation
-     * @return implemented object from interface IOperation
-     * @throws OperationNotSupportedException if operation not supported
-     */
-    public IOperation getByName(String operationName) throws OperationNotSupportedException {
-        String operationPath = OPERATIONS_PACKAGE + operationName;
-        IOperation operation;
-        try {
-            operation = (IOperation) Class.forName(operationPath).getConstructor().newInstance();
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException | ClassNotFoundException e) {
-            throw new OperationNotSupportedException();
-        }
-        return operation;
-
+    public OperationProvider(Map<String, IOperation> operationByName) {
+        this.operationByName = operationByName;
     }
 
-//    public IOperation getByName(String name) {
-//        return operationByName.get(name);
-//    }
+    public IOperation getByName(String name) {
+        return operationByName.get(name);
+    }
 
+//    /**
+//     * @param operationName name of operation
+//     * @return implemented object from interface IOperation
+//     * @throws OperationNotSupportedException if operation not supported
+//     */
+//    public IOperation getByName(String operationName) throws OperationNotSupportedException {
+//        String operationPath = OPERATIONS_PACKAGE + operationName;
+//        IOperation operation;
+//        try {
+//            operation = (IOperation) Class.forName(operationPath).getConstructor().newInstance();
+//        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException | ClassNotFoundException e) {
+//            throw new OperationNotSupportedException();
+//        }
+//        return operation;
+//
+//    }
 }
 
