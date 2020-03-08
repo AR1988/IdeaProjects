32f53ab9d621ea45c84e0233a2a63859ea1effca
AR1988 <AudiTT2012>
2020-03-08 20:06:52 +0100

    upload

---
+++ Java+/stream/07_03_20_users/src/com/company/Application.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Application {
+
+    public static void main(String[] args) {
+        LogEntry logEntry1 = new LogEntry("acc1 ", "ur1 ");
+        LogEntry logEntry3 = new LogEntry("acc1 ", "ur1 ");
+        LogEntry logEntry4 = new LogEntry("acc1 ", "ur2 ");
+        LogEntry logEntry2 = new LogEntry("acc2 ", "ur1 ");
+        LogEntry logEntry5 = new LogEntry("acc2 ", "ur1 ");
+        LogEntry logEntry6 = new LogEntry("acc2 ", "ur2 ");
+        LogEntry logEntry7 = new LogEntry("acc2 ", "ur2 ");
+        LogEntry logEntry8 = new LogEntry("acc2 ", "ur1 ");
+        LogEntry logEntry9 = new LogEntry("acc3 ", "ur1 ");
+        LogEntry logEntry10 = new LogEntry("acc3 ", "ur2 ");
+        LogEntry logEntry11 = new LogEntry("acc3 ", "ur3 ");
+
+        List<LogEntry> logEntries = new ArrayList<>(Arrays.asList(logEntry1,
+                logEntry2,
+                logEntry3,
+                logEntry4,
+                logEntry5,
+                logEntry6,
+                logEntry7,
+                logEntry8,
+                logEntry9,
+                logEntry10,
+                logEntry11));
+
+        Utils utils = new Utils();
+        //группировка по имени url.
+        System.out.println(utils.linkCliced(logEntries.stream()).toString());           //количесвто вызовов url
+        System.out.println(utils.usersByUrl(logEntries.stream()).toString());           //количесвто уникальных пользователей
+        System.out.println(utils.usersByUrl_UsersList(logEntries.stream()).toString()); //список пользователей и из количество
+    }
+}
---
+++ Java+/stream/07_03_20_users/src/com/company/LogEntry.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class LogEntry {
+    private String username;
+    private String url;
+
+    public LogEntry(String username, String url) {
+        this.username = username;
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
