2aaa3de54895ef40d5be7ce779f50ec0d1fa0bde
andre.reutow@gmail.com <andre.reutow@gmail.com>
2020-03-27 18:06:23 +0100

    using ArrayList for threads.

--- projects/20_03_20_load_balancer/20_03_20_load_balancer/src/com/company/LoadBalancer.java
+++ projects/20_03_20_load_balancer/20_03_20_load_balancer/src/com/company/LoadBalancer.java
@@ -1,13 +1,17 @@
 package com.company;
 
-import com.company.updService.ListeningOuterServer;
+import com.company.updService.CheckServerStatus;
 import com.company.updService.ListeningHandlersServer;
+import com.company.updService.ListeningOuterServer;
 
 import java.io.IOException;
 import java.net.DatagramSocket;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.PriorityQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class LoadBalancer {
 
@@ -15,22 +19,20 @@ public class LoadBalancer {
     private static final int OUTERSERVER_PORT = 2015;
 
     public static void main(String[] args) throws IOException {
-
-//        PriorityQueue<ServerInfo> source = new PriorityQueue<>();
-        ConcurrentLinkedQueue<ServerInfo> source = new ConcurrentLinkedQueue();
+        PriorityQueue<ServerInfo> source = new PriorityQueue<>();
+        Source serverInfo = new Source(source);
 
         DatagramSocket listeningServerLoad = new DatagramSocket(SERVER_PORT);
         DatagramSocket listeningOuterServer = new DatagramSocket(OUTERSERVER_PORT);
 
         List<Thread> threads = Arrays.asList(
-                new Thread(new ListeningHandlersServer(listeningServerLoad, source)),
-                new Thread(new ListeningOuterServer(listeningOuterServer, source)),
-                new Thread(new CheckServersStatus(source))
+                new Thread(new ListeningHandlersServer(listeningServerLoad, serverInfo)),
+                new Thread(new ListeningOuterServer(listeningOuterServer, serverInfo))
         );
-        ConcurrentLinkedQueue<Thread> concurrentLinkedQueue = new ConcurrentLinkedQueue(threads);
+        threads.forEach(Thread::start);
 
-        concurrentLinkedQueue.forEach(Thread::start);
-
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+        service.scheduleWithFixedDelay(new CheckServerStatus(source), 0, 3, TimeUnit.SECONDS);
     }
 }
 
