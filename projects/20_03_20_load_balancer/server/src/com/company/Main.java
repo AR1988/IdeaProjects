dd7d2563b81e7d7647367bbd52c7eb274ce95037
andre.reutow@gmail.com <andre.reutow@gmail.com>
2020-03-29 11:38:41 +0200

    add load params from config file, replace Thread.sleep with ScheduledExecutorService

--- projects/20_03_20_load_balancer/server/src/com/company/Main.java
+++ projects/20_03_20_load_balancer/server/src/com/company/Main.java
@@ -8,31 +8,47 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class Main {
     private static final String FILE_DIRECTORY = "C:/Users/andre/IdeaProjects/projects/20_03_20_load_balancer/config/config.props";
     private static final String SERVER_1_PORT = "SERVER_1_PORT";
+    private static final String SERVER_1_NAME = "SERVER_1_NAME";
+    private static final String SERVER_1_THREADS = "SERVER_1_THREADS";
+    private static final String START_LOAD_SERV_1 = "START_LOAD_SERV_1";
+    private static final String LOAD_UPDATE_TIME = "LOAD_UPDATE_TIME";
+    private static final String LOADBALANCE_PORT_FOR_SERVERS = "LOADBALANCE_PORT_FOR_SERVERS";
+    private static final String LOADBALANCE_IP = "LOADBALANCE_IP";
 
 
     public static void main(String[] args) throws IOException {
-        AtomicInteger serverLoad = new AtomicInteger(0);
-
         ConfigReader configReader = new ConfigReader(FILE_DIRECTORY);
+
         int portServer1 = Integer.parseInt(configReader.loadParamFromConfig(SERVER_1_PORT));
+        int startLoad = Integer.parseInt(configReader.loadParamFromConfig(START_LOAD_SERV_1));
+        String serverName1 = configReader.loadParamFromConfig(SERVER_1_NAME);
+        int nThreads = Integer.parseInt(configReader.loadParamFromConfig(SERVER_1_THREADS));
+        long timeForUpdate = Integer.parseInt(configReader.loadParamFromConfig(LOAD_UPDATE_TIME));
+        int loadBalancePort = Integer.parseInt(configReader.loadParamFromConfig(LOADBALANCE_PORT_FOR_SERVERS));
+        String loadBalanceIP = configReader.loadParamFromConfig(LOADBALANCE_IP);
 
+        AtomicInteger serverLoad = new AtomicInteger(startLoad);
         ServerSocket server1 = new ServerSocket(portServer1);
+        ExecutorService executor = Executors.newFixedThreadPool(nThreads);
 
-        Thread serverLoadThread = new Thread(new ServerLoad(serverLoad, portServer1, configReader));
-        serverLoadThread.start();
-
-        ExecutorService executor = Executors.newFixedThreadPool(5);
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+        service.scheduleWithFixedDelay(new ServerLoad(serverLoad,
+                portServer1,
+                loadBalancePort,
+                loadBalanceIP), timeForUpdate, timeForUpdate, TimeUnit.MILLISECONDS);
 
         while (true) {
             Socket socket = server1.accept();
             serverLoad.incrementAndGet();
-            ServerAnswer serverTask = new ServerAnswer(socket, serverLoad, 1);
+            ServerAnswer serverTask = new ServerAnswer(socket, serverLoad, serverName1);
 
             executor.execute(serverTask);
         }
