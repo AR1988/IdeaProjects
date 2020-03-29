db3514e0335833a643b79f343c3220455c5e5f99
andre.reutow@gmail.com <andre.reutow@gmail.com>
2020-03-29 09:10:53 +0200

    load param from config file and add request to reserve server

--- projects/20_03_20_load_balancer/20_03_20_load_balancer/src/com/company/updService/ListeningOuterServer.java
+++ projects/20_03_20_load_balancer/20_03_20_load_balancer/src/com/company/updService/ListeningOuterServer.java
@@ -1,46 +1,89 @@
 package com.company.updService;
 
-import com.company.ServerInfo;
 import com.company.Source;
+import com.company.configServices.ConfigReader;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
-import java.util.PriorityQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ListeningOuterServer implements Runnable {
-    Source source;
-    int port;
-    private static final int DATA_SIZE = 1024;
-    DatagramSocket socket;
+    private Source source;
+    private DatagramSocket socket;
+    int threads;
+    private int dataSize;
 
-    public ListeningOuterServer(Source source, int port) throws SocketException {
+
+    public ListeningOuterServer(Source source, int port, int threads, int dataSize) throws SocketException {
         this.source = source;
-        this.port = port;
         this.socket = new DatagramSocket(port);
+        this.threads = threads;
+        this.dataSize = dataSize;
     }
 
     @Override
     public void run() {
+        ExecutorService executorService = Executors.newFixedThreadPool(threads);
+
         while (true) {
             try {
-                byte[] dataToReceive = new byte[DATA_SIZE];
-                DatagramPacket packetIn = new DatagramPacket(dataToReceive, DATA_SIZE);
-                socket.receive(packetIn);
-                String address = source.peek().toString();
-                //send the data back
-                byte[] dataToSendBack = address.split("/")[1].getBytes();
-                DatagramPacket packetOut = new DatagramPacket(
-                        dataToSendBack,
-                        dataToSendBack.length,
-                        packetIn.getAddress(),
-                        packetIn.getPort());
-                socket.send(packetOut);
+                byte[] dataToReceive = new byte[dataSize];
+                DatagramPacket packetIn = new DatagramPacket(dataToReceive, dataSize);
+                this.socket.receive(packetIn);
+
+                SendServerToOuterServer task = new SendServerToOuterServer(source, socket, packetIn);
+
+                executorService.execute(task);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+
+}
+
+class SendServerToOuterServer implements Runnable {
+    private Source source;
+    private DatagramSocket socket;
+    private DatagramPacket packetIn;
+
+    public SendServerToOuterServer(Source source, DatagramSocket socket, DatagramPacket packetIn) {
+        this.source = source;
+        this.socket = socket;
+        this.packetIn = packetIn;
+    }
+
+    private static final String FILE_DIRECTORY = "C:/Users/andre/IdeaProjects/projects/20_03_20_load_balancer/config/config.props";
+    private static final String SERVER_0_PORT = "SERVER_0_PORT";
+    private static final String LOAD_BALANCE_IP = "LOADBALANCE_IP";
+
+    @Override
+    public void run() {
+        try {
+            //if no servers in source, then using reserve server on
+            String address = source.size() != 0 ? source.peek().toString() : getReserveServer();
+            byte[] dataToSendBack = address.split("/")[1].getBytes();
+            DatagramPacket packetOut = new DatagramPacket(
+                    dataToSendBack,
+                    dataToSendBack.length,
+                    packetIn.getAddress(),
+                    packetIn.getPort());
+
+            socket.send(packetOut);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getReserveServer() throws IOException {
+        ConfigReader configReader = new ConfigReader(FILE_DIRECTORY);
+        int portServer0 = Integer.parseInt(configReader.loadParamFromConfig(SERVER_0_PORT));
+        String reserveServerIP = configReader.loadParamFromConfig(LOAD_BALANCE_IP);
+        return "/" + reserveServerIP + "_" + portServer0;
     }
 }
 
