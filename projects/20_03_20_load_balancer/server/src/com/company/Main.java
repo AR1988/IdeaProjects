f7af8467f90120f9385b43161e7b29cb236bf85d
andre.reutow@gmail.com <andre.reutow@gmail.com>
2020-03-30 18:10:54 +0200

    add config

--- projects/20_03_20_load_balancer/server/src/com/company/Main.java
+++ projects/20_03_20_load_balancer/server/src/com/company/Main.java
@@ -14,25 +14,28 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class Main {
-    private static final String FILE_DIRECTORY = "C:/Users/andre/IdeaProjects/projects/20_03_20_load_balancer/config/config.props";
-    private static final String SERVER_1_PORT = "SERVER_1_PORT";
-    private static final String SERVER_1_NAME = "SERVER_1_NAME";
-    private static final String SERVER_1_THREADS = "SERVER_1_THREADS";
-    private static final String START_LOAD_SERV_1 = "START_LOAD_SERV_1";
+    private static final String FILE_DIRECTORY = "config/config.props";
+
+    private static final String SERVER_PORT = "SERVER_PORT";
+    private static final String SERVER_NAME = "SERVER_NAME";
+    private static final String SERVER_THREADS = "SERVER_THREADS";
+
+    private static final String START_LOAD_SERV = "START_LOAD_SERV";
     private static final String LOAD_UPDATE_TIME = "LOAD_UPDATE_TIME";
-    private static final String LOADBALANCE_PORT_FOR_SERVERS = "LOADBALANCE_PORT_FOR_SERVERS";
+
+    private static final String LOADBALANCE_PORT = "LOADBALANCE_PORT";
     private static final String LOADBALANCE_IP = "LOADBALANCE_IP";
 
 
     public static void main(String[] args) throws IOException {
         ConfigReader configReader = new ConfigReader(FILE_DIRECTORY);
 
-        int portServer1 = Integer.parseInt(configReader.loadParamFromConfig(SERVER_1_PORT));
-        int startLoad = Integer.parseInt(configReader.loadParamFromConfig(START_LOAD_SERV_1));
-        String serverName1 = configReader.loadParamFromConfig(SERVER_1_NAME);
-        int nThreads = Integer.parseInt(configReader.loadParamFromConfig(SERVER_1_THREADS));
+        int portServer1 = Integer.parseInt(configReader.loadParamFromConfig(SERVER_PORT));
+        int startLoad = Integer.parseInt(configReader.loadParamFromConfig(START_LOAD_SERV));
+        String serverName1 = configReader.loadParamFromConfig(SERVER_NAME);
+        int nThreads = Integer.parseInt(configReader.loadParamFromConfig(SERVER_THREADS));
         long timeForUpdate = Integer.parseInt(configReader.loadParamFromConfig(LOAD_UPDATE_TIME));
-        int loadBalancePort = Integer.parseInt(configReader.loadParamFromConfig(LOADBALANCE_PORT_FOR_SERVERS));
+        int loadBalancePort = Integer.parseInt(configReader.loadParamFromConfig(LOADBALANCE_PORT));
         String loadBalanceIP = configReader.loadParamFromConfig(LOADBALANCE_IP);
 
         AtomicInteger serverLoad = new AtomicInteger(startLoad);
