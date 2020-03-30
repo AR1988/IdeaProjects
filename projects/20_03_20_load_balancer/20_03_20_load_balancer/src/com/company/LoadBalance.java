da0f849f2a10f375dfefb38c49f2bf3b75fb5670
andre.reutow@gmail.com <andre.reutow@gmail.com>
2020-03-30 18:31:39 +0200

    add config

--- projects/20_03_20_load_balancer/20_03_20_load_balancer/src/com/company/LoadBalance.java
+++ projects/20_03_20_load_balancer/20_03_20_load_balancer/src/com/company/LoadBalance.java
@@ -14,7 +14,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-public class LoadBalancer {
+public class LoadBalance {
     private static final String CONFIG_FILE_DIRECTORY = "config/config.props";
 
     private static final String LOADBALANCE_PORT_FOR_SERVERS = "LOADBALANCE_PORT_FOR_SERVERS";
@@ -24,8 +24,8 @@ public class LoadBalancer {
     private static final String THREADS_FOR_LISTENING_SERVER = "THREADS_FOR_LISTENING_SERVER";
     private static final String THREADS_FOR_LISTENING_OUTERSERVER = "THREADS_FOR_LISTENING_OUTERSERVER";
 
-    private static final String SERVER_0_PORT = "SERVER_0_PORT";
-    private static final String SERVER_0_NAME = "SERVER_0_NAME";
+    private static final String RESERVE_SERVER_PORT = "RESERVE_SERVER_PORT";
+    private static final String RESERVE_SERVER_NAME = "RESERVE_SERVER_NAME";
 
     private static final String MAX_DIED_TIME_SERVERS = "MAX_DIED_TIME_SERVERS";
 
@@ -41,8 +41,8 @@ public class LoadBalancer {
         int threadsForListeningServer = Integer.parseInt(configReader.loadParamFromConfig(THREADS_FOR_LISTENING_SERVER));
         int threadsForListeningOuterServer = Integer.parseInt(configReader.loadParamFromConfig(THREADS_FOR_LISTENING_OUTERSERVER));
 
-        int reserveServerPort = Integer.parseInt(configReader.loadParamFromConfig(SERVER_0_PORT));
-        String reserveServerName = configReader.loadParamFromConfig(SERVER_0_NAME);
+        int reserveServerPort = Integer.parseInt(configReader.loadParamFromConfig(RESERVE_SERVER_PORT));
+        String reserveServerName = configReader.loadParamFromConfig(RESERVE_SERVER_NAME);
 
         long updateTime = Long.parseLong(configReader.loadParamFromConfig(MAX_DIED_TIME_SERVERS));
         PriorityQueue<ServerInfo> source = new PriorityQueue<>();
